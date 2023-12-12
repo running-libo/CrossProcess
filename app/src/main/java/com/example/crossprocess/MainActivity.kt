07c150788b7d2444f99125c007336eccbcc784d6
libo <417845639@qq.com>
2023-12-13 01:18:55 +0800

    messenger相互通信完成

--- app/src/main/java/com/example/crossprocess/MainActivity.kt
+++ app/src/main/java/com/example/crossprocess/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.Handler
 import android.os.IBinder
 import android.os.Message
 import android.os.Messenger
@@ -31,11 +32,32 @@ class MainActivity : ComponentActivity() {
                 //创建消息，通过Bundle传递数据
                 var message = Message.obtain(null, 100)
                 message.data = Bundle().apply {
-                    putString("data", "从客户端发来消息")
+                    putString("data", "服务端，我给你发送消息了")
                 }
+
+                //将客户端的Messenger对象传递给服务端，才能让双方互相通信
+                message.replyTo = mClientMessenger
 
                 //像服务端进程发送消息
                 it.send(message)
+            }
+        }
+    }
+
+    /**
+     * 客户端Messenger对象
+     */
+    private val mClientMessenger: Messenger = Messenger(MessengerHandler())
+
+    /**
+     * 用于构建客户端的Messenger对象，并处理服务端的消息
+     */
+    private class MessengerHandler : Handler() {
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                100 -> {
+                    println(msg.data.getString("data"))
+                }
             }
         }
     }
--- app/src/main/java/com/example/crossprocess/messenger/MessengerService.java
+++ app/src/main/java/com/example/crossprocess/messenger/MessengerService.java
@@ -2,10 +2,13 @@ package com.example.crossprocess.messenger;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.os.RemoteException;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
@@ -20,8 +23,27 @@ public class MessengerService extends Service {
             switch (msg.what) {
                 case 100:
                     System.out.println(msg.getData().getString("data"));
+
+                    sendToClient(msg);
                     break;
             }
+        }
+    }
+
+    /**
+     * 给客户度发送消息
+     */
+    private static void sendToClient(Message message) {
+        Messenger client = message.replyTo;  //通过 message拿到客户传递过来的 Messenger，通过该对象发送message
+        //当然，回传消息还是要通过message
+        Message msg = Message.obtain(null, 100);
+        Bundle bundle = new Bundle();
+        bundle.putString("data", "客户端, 我收到你的消息了");
+        msg.setData(bundle);
+        try {
+            client.send(msg);
+        } catch (RemoteException e) {
+            e.printStackTrace();
         }
     }
 
